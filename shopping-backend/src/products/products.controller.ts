f68fb829f9e94d3982c2b8f39ea842e78d001271
mohamedeld <mohamed.azoz20010@gmail.com>
2026-08-19 10:19:04 +0300

    add upload image

--- shopping-backend/src/products/products.controller.ts
+++ shopping-backend/src/products/products.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-gurad';
 import { CreateProductDto } from './dto/create-product.dto';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
 import { ProductsService } from './products.service';
 import { Product } from 'generated/prisma/client';
-
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productService: ProductsService) {}
@@ -18,6 +31,36 @@ export class ProductsController {
   ): Promise<Product> {
     return await this.productService.createProduct(body, user?.userId);
   }
+
+  @Post('/:id/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const originalName = file.originalname.replace(/\s+/g, '-');
+          const filename = `${uniqueSuffix}-${originalName}`;
+          cb(null, filename);
+        },
+      }),
+    }),
+  )
+  uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 500000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Param() id: string,
+    @CurrentUser() user: TokenPayload,
+  ) {}
 
   @Get('all')
   @UseGuards(JwtAuthGuard)
